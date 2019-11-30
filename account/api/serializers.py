f1c6fb573bd02f8cf7b20885823783cf6e198a85
Erdinc Kandemir <kandemire15@itu.edu.tr>
2019-12-01 02:41:10 +0300

    register method added

--- account/api/serializers.py
+++ account/api/serializers.py
@@ -1,13 +1,15 @@
-from rest_framework import serializers
+import datetime
+from rest_framework import serializers,fields
 
 from account.models import Account
 
+
 class RegistrationSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(style={'input_type':'password'},write_only=True)
-
+    date = fields.DateField(input_formats=['%Y-%m-%d'])
     class Meta:
         model = Account
-        fields = ['email', 'phone', 'password', 'password2']
+        fields = ['email', 'phone', 'name', 'surname' ,'gender', 'password', 'password2','date']
         extra_kwargs = {
            'password':{'write_only': True}
         }
@@ -16,10 +18,19 @@ class RegistrationSerializer(serializers.ModelSerializer):
         account = Account(
             email=self.validated_data['email'],
             phone=self.validated_data['phone'],
+            name=self.validated_data['name'],
+            surname=self.validated_data['surname'],
+            dob=self.validated_data['date'],
+            gender=self.validated_data['gender'],
         )
 
+        # account.dob = datetime.date(int(self.validated_data['dob'][0:4]), int(self.validated_data['dob'][5:7]), int(self.validated_data['dob'][8:10])),
         password = self.validated_data['password']
         password2 = self.validated_data['password2']
+
+
+        if account.phone > "5000000000" and account.phone < "5999999999":
+            raise serializers.ValidationError({"name": "Phone is not proper."})
 
         if password != password2:
             raise serializers.ValidationError({"password": "Passwords must match."})
--- account/models.py
+++ account/models.py
@@ -1,25 +1,37 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+import datetime
 # Create your models here.
 
 class MyAccountManager(BaseUserManager):
-    def create_user(self, email, phone, password=None):
+    def create_user(self, email, phone, name, surname, dob, gender, password=None):
         if not email:
             raise ValueError("Users must have email")
         if not phone:
             raise ValueError("Users must have phone")
+        if not name:
+            raise ValueError("Users must have name")
+        if not surname:
+            raise ValueError("Users must have surname")
+        if not dob:
+            raise ValueError("Users must have surname")
+        if not gender:
+            raise ValueError("Users must have surname")
 
         user = self.model(
             email = self.normalize_email(email),
             phone = phone,
+            name = name,
+            surname = surname,
+            dob = dob,
+            gender = gender,
         )
         user.set_password(password)
         user.save(using = self._db)
         return user
-    def create_superuser(self,email, phone, password):
+    def create_superuser(self,email, password):
         user = self.create_user(
             email=self.normalize_email(email),
-            phone=phone,
             password = password
         )
         user.is_admin = True
@@ -36,13 +48,17 @@ class Account(AbstractBaseUser):
 
     email = models.EmailField(verbose_name='email', max_length=60, unique=True)
     phone = models.CharField(verbose_name='phone', max_length=60, unique=True)
+    name = models.CharField(verbose_name='name', max_length=60, default='name')
+    surname = models.CharField(verbose_name='surname', max_length=60, default='surname')
+    dob = models.DateField(verbose_name= 'dob',default=datetime.date(1997, 10, 19))
+    gender = models.CharField(verbose_name='gender', max_length=1, default='N')
     is_admin = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['phone']
+    REQUIRED_FIELDS = ['phone', 'name', 'surname', 'dob', 'gender']
 
     objects = MyAccountManager()
 
