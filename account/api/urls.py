ec67cd0b6f2351e1d72ae652be52e1f3a5ab1a96
Erdinc Kandemir <kandemire15@itu.edu.tr>
2019-12-01 01:13:38 +0300

    mock register method added

---
+++ account/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from account.api.views import (
+    registration_view
+)
+
+app_name = 'account'
+
+urlpatterns = [
+    path('register', registration_view, name='register'),
+]
