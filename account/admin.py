0adb763f86cee77c7bcbac359bca31d7cbf42e00
keremalatepe <keremgokmenalatepe@gmail.com>
2019-12-11 12:01:00 +0300

    List businesses, list cards, add card, delete card STAM-17

--- account/admin.py
+++ account/admin.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 
-from account.models import Account,Business,BusinessAccount,BusinessToken,Card,QRCode
+from account.models import Account,Business,BusinessAccount,BusinessToken
 
 
 admin.site.register(Account)
 admin.site.register(Business)
 admin.site.register(BusinessAccount)
 admin.site.register(BusinessToken)
-admin.site.register(QRCode)
 
 # Register your models here.
--- account/api/serializers.py
+++ account/api/serializers.py
@@ -1,20 +1,17 @@
 import datetime
-from abc import ABC
+from rest_framework import serializers,fields
 
-from rest_framework import serializers, fields
+from account.models import Account, Business, BusinessAccount
 
-from account.models import Account, Business, BusinessAccount, Card, BusinessToken, QRCode
-from rest_framework.authtoken.models import Token
 
 class RegistrationSerializer(serializers.ModelSerializer):
-    password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+    password2 = serializers.CharField(style={'input_type':'password'},write_only=True)
     date = fields.DateField(input_formats=['%Y-%m-%d'])
-
     class Meta:
         model = Account
-        fields = ['email', 'phone', 'name', 'surname', 'gender', 'password', 'password2', 'date']
+        fields = ['email', 'phone', 'name', 'surname' ,'gender', 'password', 'password2','date']
         extra_kwargs = {
-            'password': {'write_only': True}
+           'password':{'write_only': True}
         }
 
     def save(self):
@@ -27,8 +24,10 @@ class RegistrationSerializer(serializers.ModelSerializer):
             gender=self.validated_data['gender'],
         )
 
+        # account.dob = datetime.date(int(self.validated_data['dob'][0:4]), int(self.validated_data['dob'][5:7]), int(self.validated_data['dob'][8:10])),
         password = self.validated_data['password']
         password2 = self.validated_data['password2']
+
 
         if int(account.phone) < 5000000000 or int(account.phone) > 5999999999:
             raise serializers.ValidationError({"name": "Phone is not proper."})
@@ -39,27 +38,26 @@ class RegistrationSerializer(serializers.ModelSerializer):
         account.save()
         return account
 
-
 class BusinessRegisterSerializer(serializers.Serializer):
     name = serializers.CharField(required=True, allow_blank=False, max_length=100)
     email = serializers.EmailField(required=True, allow_blank=False)
     card_image = serializers.CharField(required=True, allow_blank=False, max_length=100)
     stamp_need = serializers.IntegerField(required=True)
 
-
-    def create(self, validated_data):
+    def create(self,validated_data):
         return Business.objects.create(**validated_data)
+
+
 
 
 class BusinessUserRegistrationSerializer(serializers.ModelSerializer):
     business_email = serializers.EmailField()
-    password2 = serializers.CharField(style={'input_type': 'password'}, write_only=True)
-
+    password2 = serializers.CharField(style={'input_type':'password'},write_only=True)
     class Meta:
         model = BusinessAccount
-        fields = ['email', 'business_email', 'permission', 'password', 'password2']
+        fields = ['email','business_email', 'permission','password','password2']
         extra_kwargs = {
-            'password': {'write_only': True}
+           'password':{'write_only': True}
         }
 
     def save(self):
@@ -70,8 +68,10 @@ class BusinessUserRegistrationSerializer(serializers.ModelSerializer):
             permission=0,
         )
 
+        # account.dob = datetime.date(int(self.validated_data['dob'][0:4]), int(self.validated_data['dob'][5:7]), int(self.validated_data['dob'][8:10])),
         password = self.validated_data['password']
         password2 = self.validated_data['password2']
+
 
         if password != password2:
             raise serializers.ValidationError({"password": "Passwords must match."})
@@ -82,29 +82,8 @@ class BusinessUserRegistrationSerializer(serializers.ModelSerializer):
 
 
 
-
-class QRCodeSerializer(serializers.Serializer):
-    business_id = serializers.CharField(required=True, allow_blank=False, max_length=100)
-    customer_id = serializers.CharField(required=True, allow_blank=False, max_length=100)
-    qr_code = serializers.CharField(required=True, allow_blank=False, max_length=36)
-
-    def save(self):
-        customer = Account.objects.get(id=self.validated_data['customer_id'])
-        business = Business.objects.get(id=self.validated_data['business_id'])
-        qr_code = QRCode(customer=customer,
-                    business=business,
-                    qr_code=self.validated_data['qr_code'])
-
-        qr_code.save()
-        return qr_code
-
-
-
-    def create(self, validated_data):
-        return QRCode.objects.create(**validated_data)
-
-
 class ChangePasswordSerializer(serializers.Serializer):
-    old_password = serializers.CharField(style={'input_type': 'password'}, required=True)
-    new_password = serializers.CharField(style={'input_type': 'password'}, required=True)
-    confirm_new_password = serializers.CharField(style={'input_type': 'password'}, required=True)
+
+	old_password 				= serializers.CharField(style={'input_type':'password'},required=True)
+	new_password 				= serializers.CharField(style={'input_type':'password'},required=True)
+	confirm_new_password 		= serializers.CharField(style={'input_type':'password'},required=True)
