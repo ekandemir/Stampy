1121750a2a6c51c0ff47ca3abeb3162ee57b4e53
keremalatepe <keremgokmen.alatepe@hemenis.com>
2019-12-22 15:25:40 +0300

    STAM-31, register test done

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from account.models import (Account,
+                            Business,
+                            BusinessToken,
+                            BusinessAccount,
+                            Card,
+                            QRCode,
+                            Offer,
+                            StampLog,
+                            AddDeleteCardLog)
 
 # Create your tests here.
+class TestAccount(TestCase):
+    def setUp(self):
+        self.account = Account.objects.create_user(email="deneme@email.com",
+                                                   phone="5535535353",
+                                                   name="deneme",
+                                                   surname="denemeoglu",
+                                                   password="123",
+                                                   dob="2019-9-11",
+                                                   gender="E")
+    def test_username(self):
+        self.assertEqual(self.account.name, "deneme")
